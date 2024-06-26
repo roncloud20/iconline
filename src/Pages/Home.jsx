69dc5140210d46f99d9c58191b20a291195c7bc6
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2024-06-26 18:34:58 +0100

    adding typography

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import Header from "../Components/Header";
+
+export default function Home() {
+    return (
+        <>
+        <Header/>
+        <h1 className="">Welcome To Iconline Online Store</h1>
+        </>
+    )
+}
